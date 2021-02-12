2f474bbe47c193f359f4bee80399e97605bfd498
vladyslav.telehin <vladyslav.telehin@xenial.com>
2021-02-12 20:52:01 +0200

    Cache invalidation.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     host: process.env.IS_PROD ? "https://dotnet-code-judge-i5fgk.ondigitalocean.app" : "http://localhost:3000",
-    instancesCount: process.env.IS_PROD ? 4 : 1
+    instancesCount: process.env.IS_PROD ? 2 : 1
 };
